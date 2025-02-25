20d3944a66870935f621441012a20669e64ce7d3
Madhubaalika <madhubaalika@gmail.com>
2025-02-25 21:55:56 +0530

    updated services page

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,121 +1,85 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Rservices from "./Rservices";
 import Sservices from "./Sservices";
 import Cservices from "./Cservices";
-import Gservices from "./Gservices";
-
+import { MdMiscellaneousServices } from 'react-icons/md';
+import { BiSearchAlt } from 'react-icons/bi';
+import { GiShakingHands } from 'react-icons/gi';
+import { FaGraduationCap } from 'react-icons/fa';
 
 function Services() {
   const [selectedTopic, setSelectedTopic] = useState("For Researchers");
-  const [isMobileScreen, setIsMobileScreen] = useState(false);
-useEffect(() => {
-    const handleResize = () => {
-      setIsMobileScreen(window.innerWidth <= 768);
-    };
 
-    window.addEventListener("resize", handleResize);
-    handleResize();
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const topics = [
-    {
-      id: "For Researchers",
-      title: "For Researchers",
-      content: (
-       <Rservices/>
-      )
-    },
-    {
-      id: "For Students",
-      title: "For Students",
-      content: (
-        <Sservices/>
-      )
-    },
-    {
-      id: "For Company",
-      title: "For Company",
-      content: (
-        <Cservices/>
-      )
-    },
-  ];
-
-  const currentTopic = topics.find((topic) => topic.id === selectedTopic);
+  const topics = {
+    "For Researchers": <Rservices />, 
+    "For Students": <Sservices />, 
+    "For Company": <Cservices />
+  };
 
   return (
-    <div className="bg-gray-50">
-      <Gservices/>
+    <div className="bg-gray-50 flex flex-col items-center pt-7">
+      {/* Header */}
+      <div className="bg-white rounded-lg p-4 flex flex-col items-start justify-center w-10/12 md:w-3/5 mb-4 text-center">
+        <div className="flex items-center justify-center mb-4">
+          <div className="bg-white rounded-full p-4 border-white border-2 flex items-center justify-center w-20 h-20 hover:border-black transition-colors duration-300 ease-in-out">
+            <MdMiscellaneousServices className="text-black text-6xl hover:text-[#2d2f2ef5] transition-colors duration-300 ease-in-out" />
+          </div>
+          <h3 className="text-3xl font-bold tracking-wider text-[#232627] font-raleway pl-7 underline underline-offset-8 decoration-blue-200 decoration-1">OUR SERVICES</h3>
+        </div>
+        <p className="text-[#2D2D2D] text-lg font-roboto">
+        We want researchers, students, and companies to be a part of this journey in exploring how immersive visual
+            technology can be a beneficial tool for a multitude of functions, be it scientific, industrial, marketing, or
+            entertainment. We offer different services from tailor-made exclusive research projects to short-term, easy
+            access projects.
+                    </p>
+      </div>
 
-      {/* Main Content Section */}
-      {!isMobileScreen ?
-           ( <><div className="max-w-7xl mx-auto p-8">
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-      {/* Left Section */}
-    <div className="flex flex-col space-y-4 md:col-span-1 md:col-span-1">
-    {topics.map((topic) => (
-        <motion.div
-          key={topic.id}
-          onClick={() => setSelectedTopic(topic.id)}
-          className={`p-4 rounded-lg shadow-md cursor-pointer ${
-            selectedTopic === topic.id
-              ? "bg-blue-50 text-[#1b263b]"
-              : "bg-white text-[#1b263b]"
-          } hover:bg-gray-200`}
-          whileHover={{ scale: 1.05 }}
-        >
-          <h3 className="text-lg font-semibold">{topic.title}</h3>
-        </motion.div>
+      {/* Icons Section */}
+      <div className="flex flex-wrap justify-center">
+        {/* Researcher Icon */}
+        <div className="bg-white rounded-lg p-4 flex flex-col items-center justify-center w-64 mx-4 my-4 cursor-pointer"
+          onClick={() => setSelectedTopic("For Researchers")}>
+          <div className="rounded-full bg-[#47D5CD] p-4 hover:scale-110 transition-all duration-300 ease-in-out">
+            <BiSearchAlt className="text-7xl text-white" />
+          </div>
+          <h5 className="text-sm font-semibold tracking-wider text-[#232627] font-raleway mb-4 pt-5"> FOR RESEARCHERS </h5>
+          <p className="text-[#2D2D2D] text-center text-sm font-roboto">Research Collaboration</p>
+        </div>
 
-      ))}
-    </div>
-    {/* Right Section */}
-    <motion.div
-      className="bg-white shadow-lg rounded-lg p-6 md:col-span-3"
-      key={currentTopic.id}
-      initial={{ opacity: 0, x: 50 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <h2 className="text-2xl font-bold text-blue-900 mb-4">
-        {currentTopic.title}
-      </h2>
-      {currentTopic.content}
-    </motion.div> 
+        {/* Student Icon */}
+        <div className="bg-white rounded-lg p-4 flex flex-col items-center justify-center w-64 mx-4 my-4 cursor-pointer"
+          onClick={() => setSelectedTopic("For Students")}>
+          <div className="rounded-full bg-[#31AF66F5] p-4 hover:scale-110 transition-all duration-300 ease-in-out">
+            <FaGraduationCap className="text-7xl text-white" />
+          </div>
+          <h5 className="text-sm font-semibold tracking-wider text-[#232627] font-raleway mb-4 pt-5"> FOR STUDENTS </h5>
+          <p className="text-[#2D2D2D] text-center text-sm font-roboto">Hands-on Student Co-operation</p>
+        </div>
 
-  </div>
+        {/* Company Icon */}
+        <div className="bg-white rounded-lg p-4 flex flex-col items-center justify-center w-64 mx-4 my-4 cursor-pointer"
+          onClick={() => setSelectedTopic("For Company")}>
+          <div className="rounded-full bg-[#EFA700] p-4 hover:scale-110 transition-all duration-300 ease-in-out">
+            <GiShakingHands className="text-7xl text-white" />
+          </div>
+          <h5 className="text-sm font-semibold tracking-wider text-[#232627] font-raleway mb-4 pt-5">FOR COMPANIES</h5>
+          <p className="text-[#2D2D2D] text-center text-sm font-roboto">Collaboration with Companies</p>
+        </div>
+      </div>
 
-</div></>)
-:
-(<><div className="max-w-7xl mx-auto p-8">
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-      {/* Left Section */}
-    <div className="flex flex-col space-y-4 md:col-span-1 md:col-span-1">
-    {topics.map((topic) => (
-        <motion.div
-          key={topic.id}
-          onClick={() => setSelectedTopic(topic.id)}
-          className={`p-4 rounded-lg shadow-md cursor-pointer ${
-            selectedTopic === topic.id
-              ? "bg-blue-50 text-[#1b263b]"
-              : "bg-white text-[#1b263b]"
-          } hover:bg-gray-200`}
-          whileHover={{ scale: 1.05 }}
-        >
-          <h3 className="text-lg font-semibold">{topic.title}</h3>
-        </motion.div>
-      ))}
-    </div>
-  </div>
-</div>
-{currentTopic.content}
-</>
-)}
+      {/* Content Section */}
+      <div className="bg-gray-50 flex flex-col items-center p-6">
+      <motion.div
+        className="bg-white shadow-lg rounded-lg p-8 w-full max-w-7xl mt-8"
+        key={selectedTopic}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {topics[selectedTopic]}
+      </motion.div>
+      </div>
     </div>
   );
 }
