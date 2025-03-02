34172a446ab32022487452b926813341bd3a0c69
Madhubaalika <madhubaalika@gmail.com>
2025-03-02 11:11:12 +0530

    Updated EC section

--- src/components/ExecutiveCommittee.jsx
+++ src/components/ExecutiveCommittee.jsx
@@ -1,9 +1,9 @@
 import { HoverEffect } from "./ui/card-hover-effect";
-import Manivannan from "../src/assets/ExecutiveCommitee/Manivannan.webp";
-import AnilKumar from "../src/assets/ExecutiveCommitee/AnilKumar.webp";
-import Robert from "../src/assets/ExecutiveCommitee/Robert.webp";
-import Bharathy from "../src/assets/ExecutiveCommitee/Bharathy.webp";
-import Hariharan from "../src/assets/ExecutiveCommitee/Hariharan.webp";
+import Manivannan from "../assets/ExecutiveCommitee/Manivannan.webp";
+import AnilKumar from "../assets/ExecutiveCommitee/AnilKumar.webp";
+import Robert from "../assets/ExecutiveCommitee/Robert.webp";
+import Bharathy from "../assets/ExecutiveCommitee/Bharathy.webp";
+import Hariharan from "../assets/ExecutiveCommitee/Hariharan.webp";
 
 
 const ExecutiveCommittee = () => {
@@ -13,35 +13,35 @@ const ExecutiveCommittee = () => {
       position: "Professor & Director",
       description: "Department of Applied Mechanics,",
       address: "Touch Lab IIT Madras",
-      image: {Manivannan}, 
+      image: Manivannan, 
     },
     {
       name: "Mr. Anil Kumar P N",
       position: "Vice President",
       description: "Delivery Head & Development Center Head",
       address: "Infosys, Chennai",
-      image: {AnilKumar},
+      image: AnilKumar,
     },
     {
       name: "Mr. A. Robert J Ravi, ITS",
       position: "Chief Technology Officer (CTO)",
       description: "Department of IT",
       address: "Government of Tamil Nadu, Chennai",
-      image: {Robert},
+      image: Robert,
     },
     {
       name: "Dr. CSS Bharathy",
       position: "Founder & Industry Certified 4.0 Professional",
       description: "Fusion VR",
       address: "Chennai",
-      image: {Bharathy},
+      image: Bharathy,
     },
     {
       name: "Dr. P. Hariharan, Ph.D",
       position: "Professor, Dept of Manufacturing Engineering, College of Engineering",
       description: "Director, Centre for Constituent Colleges",
       address: "Anna University, Chennai",
-    image: {Hariharan},
+    image: Hariharan,
     },
     // Add more committee members here
   ];
